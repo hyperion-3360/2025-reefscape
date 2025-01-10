cc5ad80f238a811940f78321943fdb7d2620f59a
Victoria Dalton <victoria_m_dalton@hotmail.com>
2025-01-10 16:40:49 -0500

    finished instructions for l1 shooter

--- src/main/java/frc/robot/subsystems/L1Shooter.java
+++ src/main/java/frc/robot/subsystems/L1Shooter.java
@@ -1,13 +1,14 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class L1Shooter extends SubsystemBase{
-
+/**1 Servo **/
 public L1Shooter(){
-
+//To create Servo use : public Servo m_exemple = new Servo(kidexemple);
 }
- @Override   
+ @Override
 public void periodic(){
 
 }
