5e48711886bd7fe8c030ed933cc376108a18c6c3
Gilles Hache <81245336+ghstingray@users.noreply.github.com>
2025-01-17 20:41:25 -0500

    Adding smart dashboard to climber (#24)

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -8,8 +8,8 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.DriverStation;
-// import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
 import frc.robot.Constants.SubsystemInfo;
@@ -20,19 +20,22 @@ import frc.robot.Constants.climberAction;
 // pour eviter un mouvement par accident du pilot
 public class Climber extends SubsystemBase {
 
+  private double kDt = ClimberConstants.kDt;
+  private double kMaxVelocity = ClimberConstants.kMaxAcceleration;
+  private double kMaxAcceleration = ClimberConstants.kMaxAcceleration;
+  private double kP = ClimberConstants.kP;
+  private double kI = ClimberConstants.kI;
+  private double kD = ClimberConstants.kD;
+  private double kS = ClimberConstants.kS;
+  private double kG = ClimberConstants.kG;
+  private double kV = ClimberConstants.kG;
+
   private final TrapezoidProfile.Constraints m_constraints =
-      new TrapezoidProfile.Constraints(
-          ClimberConstants.kMaxVelocity, ClimberConstants.kMaxAcceleration);
+      new TrapezoidProfile.Constraints(kMaxVelocity, kMaxAcceleration);
   private TalonFX m_climberMotor = new TalonFX(SubsystemInfo.kClimberMotorID);
   private final ProfiledPIDController m_controller =
-      new ProfiledPIDController(
-          ClimberConstants.kP,
-          ClimberConstants.kI,
-          ClimberConstants.kD,
-          m_constraints,
-          ClimberConstants.kDt);
-  private final ArmFeedforward m_feedforward =
-      new ArmFeedforward(ClimberConstants.kS, ClimberConstants.kG, ClimberConstants.kV);
+      new ProfiledPIDController(kP, kI, kD, m_constraints, kDt);
+  private final ArmFeedforward m_feedforward = new ArmFeedforward(kS, kG, kV);
 
   private static double GrabPosition = 30;
   private static double LiftPosition = 0;
@@ -64,11 +67,6 @@ public class Climber extends SubsystemBase {
     }
   }
 
-  public void stop() {
-    // stops the motors
-    m_climberMotor.stopMotor();
-  }
-
   @Override
   public void periodic() {
     if (DriverStation.isEnabled()) {
@@ -79,5 +77,28 @@ public class Climber extends SubsystemBase {
                   m_climberMotor.getPosition().getValueAsDouble(),
                   m_climberMotor.getVelocity().getValueAsDouble()));
     }
+  }
+
+  public Double getGrabTarget() {
+    return GrabPosition;
+  }
+
+  public void setGrabTarget(Double value) {
+    GrabPosition = value;
+  }
+
+  public Double getLiftTarget() {
+    return LiftPosition;
+  }
+
+  public void setLiftTarget(Double value) {
+    LiftPosition = value;
+  }
+
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    builder.setSmartDashboardType("Climber");
+    builder.addDoubleProperty("GrabTarget", this::getGrabTarget, this::setGrabTarget);
+    builder.addDoubleProperty("LiftTarget", this::getLiftTarget, this::setLiftTarget);
   }
 }
