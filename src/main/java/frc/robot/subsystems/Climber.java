3389fa20f40080f4fc89aa104f644affe94b9c66
ghstingray <ghstingray@gmail.com>
2025-01-28 18:10:27 -0500

    removing not needed line in sendable

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -123,7 +123,6 @@ public class Climber extends SubsystemBase {
     builder.setSmartDashboardType("RobotPreferences");
     builder.addDoubleProperty("GrabTarget", this::getGrabTarget, this::setGrabTarget);
     builder.addDoubleProperty("LiftTarget", this::getLiftTarget, this::setLiftTarget);
-    // builder.addDoubleProperty("LiftTarget2", this::getLiftTarget, this::setLiftTarget);
   }
 
   // This is for test mode
