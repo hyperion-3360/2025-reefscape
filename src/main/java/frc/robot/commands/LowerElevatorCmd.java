e5b8445cbf6e2a206dcdaf29c6cbef311971d50b
Igor-Volkov3360 <igorvolkovtravail@gmail.com>
2025-02-14 17:28:44 -0500

    tuned the elevator and added a patch to not slam the elevator

--- src/main/java/frc/robot/commands/LowerElevatorCmd.java
+++ src/main/java/frc/robot/commands/LowerElevatorCmd.java
@@ -16,6 +16,10 @@ public class LowerElevatorCmd extends SequentialCommandGroup {
     addCommands(
         Commands.runOnce(() -> m_leds.SetPattern(Pattern.ELEVATOR)),
         Commands.runOnce(() -> m_shooter.closeBlocker()),
+        Commands.runOnce(() -> m_elevator.SetHeight(desiredHeight.DONTPOUND))
+            .unless(() -> !m_elevator.getTargetHeight().equals(desiredHeight.L4)),
+        new WaitCommand(1.4)
+            .unless(() -> !m_elevator.getTargetHeight().equals(desiredHeight.DONTPOUND)),
         Commands.runOnce(() -> m_elevator.SetHeight(desiredHeight.LOW)),
         new WaitCommand(1.5),
         Commands.runOnce(() -> m_leds.SetPattern(Pattern.IDLE)));
