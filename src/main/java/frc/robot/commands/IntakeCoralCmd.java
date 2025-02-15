ad38e575c4cd173df04052de6735ccf208e7488a
Victoria Dalton <victoria_m_dalton@hotmail.com>
2025-02-15 15:51:34 -0500

    fixed led timing issue

--- src/main/java/frc/robot/commands/IntakeCoralCmd.java
+++ src/main/java/frc/robot/commands/IntakeCoralCmd.java
@@ -24,9 +24,10 @@ public class IntakeCoralCmd extends SequentialCommandGroup {
         Commands.runOnce(() -> m_shooter.closeBlocker()),
         Commands.runOnce(() -> m_leds.SetPattern(Pattern.INTAKE)),
         Commands.run(() -> m_shooter.setIntake()).until(() -> m_shooter.isCoralIn()),
+        Commands.runOnce(() -> m_leds.SetPattern(Pattern.READY)),
         new WaitCommand(1.2),
         Commands.runOnce(() -> m_shooter.stop()),
-        Commands.runOnce(() -> m_elevator.SetHeight(desiredHeight.LOW)),
-        Commands.runOnce(() -> m_leds.SetPattern(Pattern.READY)));
+        Commands.runOnce(() -> m_elevator.SetHeight(desiredHeight.LOW))
+        );
   }
 }
