c2307dd353c9743ecbfd0352975075f25ad07728
rose <rosemarie_dalton@hotmail.com>
2025-02-28 16:59:10 -0500

    add file that will ocntain waypoints to be used in auto

---
+++ src/main/java/frc/robot/Auto/AutoWaypoints.java
@@ -0,0 +1,77 @@
+package frc.robot.Auto;
+
+public class AutoWaypoints {
+
+    public class BlueAlliance {
+
+        public class LeftSide {
+
+            public class pegWaypoints {
+
+            }
+
+            public class stopPathplannerWaypoint {
+
+            }
+
+            public class feederWaypoints {
+
+            }
+
+        }
+
+        public class RightSide {
+
+            public class pegWaypoints {
+
+            }
+
+            public class stopPathplannerWaypoint {
+
+            }
+
+            public class feederWaypoints {
+
+            }
+
+        }
+
+    }
+
+    public class RedAlliance {
+
+        public class LeftSide {
+
+            public class pegWaypoints {
+
+            }
+
+            public class stopPathplannerWaypoint {
+
+            }
+
+            public class feederWaypoints {
+
+            }
+
+        }
+
+        public class RightSide {
+
+            public class pegWaypoints {
+
+            }
+
+            public class stopPathplannerWaypoint {
+
+            }
+
+            public class feederWaypoints {
+
+            }
+
+        }
+
+    }
+
+}
