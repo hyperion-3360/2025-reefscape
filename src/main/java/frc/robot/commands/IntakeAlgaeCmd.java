099c32b9d1d5612b0ba63ce3c210834890fcf5ba
Igor-Volkov3360 <igorvolkovtravail@gmail.com>
2025-03-13 21:42:50 -0400

    added net and algae intake

--- src/main/java/frc/robot/commands/IntakeAlgaeCmd.java
+++ src/main/java/frc/robot/commands/IntakeAlgaeCmd.java
@@ -4,17 +4,12 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.apriltag.AprilTag;
-import edu.wpi.first.apriltag.AprilTagDetection;
-import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.estimator.PoseEstimator;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.Auto.PathfindingV2;
 import frc.robot.subsystems.AlgaeIntake;
 import frc.robot.subsystems.AlgaeIntake.elevation;
 import frc.robot.subsystems.AlgaeIntake.shooting;
@@ -22,8 +17,6 @@ import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Elevator.desiredHeight;
 import frc.robot.subsystems.leds.LEDs;
 import frc.robot.subsystems.leds.LEDs.Pattern;
-import frc.robot.subsystems.swerve.Swerve;
-import frc.robot.vision.Selection;
 
 public class IntakeAlgaeCmd extends SequentialCommandGroup {
   AlgaeIntake m_algaeIntake;
@@ -68,21 +61,22 @@ public class IntakeAlgaeCmd extends SequentialCommandGroup {
         Commands.runOnce(
             () -> m_algaeIntake.setShootingSpeed(AlgaeIntake.shooting.STORING), m_algaeIntake));
   }
-/**
- * constructor to construct a algae intake command for the reef
- * @param m_algaeIntake
- * @param m_leds
- * @param m_elevator
- * @param height
- */
+
+  /**
+   * constructor to construct a algae intake command for the reef
+   *
+   * @param m_algaeIntake
+   * @param m_leds
+   * @param m_elevator
+   * @param height
+   */
   public IntakeAlgaeCmd(
-      AlgaeIntake m_algaeIntake, LEDs m_leds, Elevator m_elevator, desiredHeight height, Swerve m_swerve, Selection m_selector) {
+      AlgaeIntake m_algaeIntake, LEDs m_leds, Elevator m_elevator, desiredHeight height) {
     addRequirements(m_algaeIntake);
     addRequirements(m_leds);
     addRequirements(m_elevator);
     m_height = height;
     addCommands(
-        Commands.runOnce(() -> m_swerve.drivetoTarget(m_selector.getDesiredposeAlgae())),
         Commands.runOnce(() -> m_leds.SetPattern(Pattern.ELEVATOR)),
         Commands.runOnce(() -> m_elevator.SetHeight(height)),
         Commands.runOnce(() -> m_algaeIntake.setShootingAngle(AlgaeIntake.elevation.FLOOR)),
