56b468c415b24370cd3e51f3b445e93ff9378f4e
simonjub <78098752+simonjub@users.noreply.github.com>
2025-03-23 14:21:09 -0400

    Fix algae arms did not come back up on algae intake cancel

--- src/main/java/frc/robot/commands/LowerElevatorCmd.java
+++ src/main/java/frc/robot/commands/LowerElevatorCmd.java
@@ -1,10 +1,12 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.AlgaeIntake;
+import frc.robot.subsystems.AlgaeIntake.elevation;
 import frc.robot.subsystems.AlgaeIntake.shooting;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Elevator.desiredHeight;
@@ -28,11 +30,15 @@ public class LowerElevatorCmd extends SequentialCommandGroup {
     addCommands(
         new InstantCommand(() -> m_swerve.disableDriveToTarget()),
         Commands.runOnce(() -> m_shooter.setShoot(shootSpeed.STOP)),
-        Commands.runOnce(() -> m_algaeIntake.setShootingSpeed(shooting.STORED)),
+        new ConditionalCommand(
+            Commands.runOnce(() -> m_algaeIntake.setShootingSpeed(shooting.STORING)),
+            Commands.runOnce(() -> m_algaeIntake.setShootingSpeed(shooting.STORED)),
+            () -> m_algaeIntake.sensorTriggered()),
         Commands.runOnce(() -> m_leds.SetPattern(Pattern.ELEVATOR)),
         Commands.runOnce(() -> m_shooter.closeBlocker()),
         Commands.runOnce(() -> m_shooter.stop()),
         Commands.runOnce(() -> m_elevator.SetHeight(desiredHeight.LOW)),
+        Commands.runOnce(() -> m_algaeIntake.setShootingAngle(elevation.NET)),
         new WaitCommand(1.5),
         Commands.runOnce(() -> m_leds.SetPattern(Pattern.IDLE)));
   }
